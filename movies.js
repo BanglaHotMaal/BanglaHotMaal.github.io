4b198b8fc1b46251174043fcb905e67f71aa7bda
dk65bst <dk65bst@gmail.com>
2026-09-25 10:23:21 +0600

    Add initial movie list to movies.js

---
+++ movies.js
@@ -0,0 +1,50 @@
+const movies = [
+  {
+    title: "The Last Journey",
+    category: "Action",
+    year: "2026",
+    quality: "HD",
+    poster: "",
+    video: ""
+  },
+  {
+    title: "Hidden Story",
+    category: "Drama",
+    year: "2026",
+    quality: "HD",
+    poster: "",
+    video: ""
+  },
+  {
+    title: "Funny Weekend",
+    category: "Comedy",
+    year: "2026",
+    quality: "HD",
+    poster: "",
+    video: ""
+  },
+  {
+    title: "Dark Night",
+    category: "Thriller",
+    year: "2026",
+    quality: "HD",
+    poster: "",
+    video: ""
+  },
+  {
+    title: "Future World",
+    category: "Sci-Fi",
+    year: "2026",
+    quality: "HD",
+    poster: "",
+    video: ""
+  },
+  {
+    title: "Magic World",
+    category: "Animation",
+    year: "2026",
+    quality: "HD",
+    poster: "",
+    video: ""
+  }
+];
